39b4472442fb92bb947b5babed394a7b934db531
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-08-09 15:36:27 -0700

    optimized gravatar pic

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+  def gravatar_pic
+    email = "daniela.grossmann@gmail.com"
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: "picture of Daniela Grossmann", class: "photo")
+  end
 end
