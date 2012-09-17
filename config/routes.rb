ebab45f60536527b5b47e67e3c966174cc95de1d
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-09-16 20:30:34 -0700

    Add project Black Jack

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ AboutMe::Application.routes.draw do
   root to: 'static_pages#index'
   
   match '/project', to: 'static_pages#projects'
+  match '/blackjack', to: 'static_pages#blackjack'
 
  
 end
